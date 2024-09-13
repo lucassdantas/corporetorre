137c08ddf440111799d3f4fbb9720cc47770f854
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-13 00:48:15 -0300

    fix footer responsivity

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -8,13 +8,13 @@ import personRelaxing from '@/assets/pessoa-relaxando-corporetorre.jpg'
 export const Contact = () => {
   return (
     <Section className='bg-gray-200 py-24' id='contato'>
-      <div className='flex rounded-xl gap-8'>
-        <div className='w-1/2 flex flex-col bg-white rounded-xl shadow-md  p-6'>
+      <div className='flex lg:flex-row flex-col-reverse rounded-xl gap-8'>
+        <div className='w-full lg:w-1/2 flex flex-col bg-white rounded-xl shadow-md  p-6'>
           <Title className=''>Fale conosco</Title>
           <p className='mb-4'>Envie-nos uma mensagem, responderemos o quanto antes</p>
           <ContactForm/>
         </div>  
-        <div className='w-1/2 flex flex-col justify-center p-6 '>
+        <div className='w-full lg:w-1/2 flex flex-col justify-center p-6 '>
           <Image src={personRelaxing} alt='logotipo' className='rounded-xl shadow-lg h-[80%] object-cover'/>
         </div>  
       </div>
