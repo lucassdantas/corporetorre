e6cbd93073634bf61b068a186d716a18a6ff5a5f
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-13 01:13:35 -0300

    fix wpp button on mobile

--- src/components/FloatButton/index.tsx
+++ src/components/FloatButton/index.tsx
@@ -1,12 +1,17 @@
-import { whatsApp } from "@/utils/informations/contact-info"
-import { BsWhatsapp } from "react-icons/bs"
+import { whatsApp } from "@/utils/informations/contact-info";
+import { BsWhatsapp } from "react-icons/bs";
 
 export const FloatButton = () => {
   return (
-    <a href={whatsApp.url} target='_blank' className='fixed lg:bottom-4 lg:right-4 sm:bottom-12 sm:right-12 right-0 transition ease-in-out hover:scale-110 '>
-      <div className='bg-green-wpp p-4 w-fit rounded-full text-white cursor-pointer'>
-        <BsWhatsapp className='text-4xl'/>
+    <a
+      href={whatsApp.url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-4 right-4 lg:bottom-4 lg:right-4 sm:bottom-12 sm:right-12 transition ease-in-out hover:scale-110 z-50"
+    >
+      <div className="bg-green-wpp p-4 w-fit rounded-full text-white cursor-pointer">
+        <BsWhatsapp className="text-4xl" />
       </div>
     </a>
-  )
-}
+  );
+};
