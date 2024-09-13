ddd4979599169912aff58f9df06fd72ac98b768b
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-12 22:14:12 -0300

    video and img working on services cards

--- src/components/ServiceCard/index.tsx
+++ src/components/ServiceCard/index.tsx
@@ -11,9 +11,9 @@ export const ServiceCard = ({media, name, description, isMediaVideo, mediaUrl, c
   return (
     <div className={`flex flex-col justify-between bg-white rounded-lg p-4 text-black gap-4 ${className}`}>
       <div>
-        {!isMediaVideo && media && <Image src={media} alt={`Serviço de ${name}`} className='rounded-lg'/>}
-        {isMediaVideo && mediaUrl &&  <video controls className="w-full h-auto rounded-lg mb-6 shadow-lg"><source src={mediaUrl} type="video/mp4" ></source></video>}
-        <h3 className='font-bold text-lg'>{name}</h3>
+        {!isMediaVideo && media    && <Image src={media} alt={`Serviço de ${name}`} className='rounded-lg h-[284px] object-cover'/>}
+        {isMediaVideo  && mediaUrl &&  <video controls className="w-full h-[284px] object-cover rounded-lg mb-6 shadow-lg"><source src={mediaUrl} type="video/mp4" ></source></video>}
+        <h3 className='font-bold text-xl my-4'>{name}</h3>
         <p>{description}</p>
       </div>
       <Button/>
